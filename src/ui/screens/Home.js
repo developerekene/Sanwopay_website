143e5c28854a0732208192fb77c8b561a2924aec
Ekenedilichukwu <ekenedilichukwu@Ekenedilichukwus-MacBook-Pro.local>
2022-09-22 00:40:07 +0100

    Home page built update 2

--- src/ui/screens/Home.js
+++ src/ui/screens/Home.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
+import Accordion from "react-bootstrap/Accordion";
 
 export const Home = () => {
   return (
@@ -176,6 +177,184 @@ export const Home = () => {
         </div>
         <div>
           <Image src={require("../../images/Rectangle5.png")} />
+        </div>
+      </div>
+      {
+        // How it works section
+      }
+      <div
+        style={{
+          paddingLeft: 100,
+          paddingRight: 100,
+          paddingTop: 50,
+          fontFamily: "Work Sans",
+        }}
+      >
+        <h1
+          style={{
+            textAlign: "center",
+            marginBottom: 40,
+            fontSize: 40,
+            fontWeight: "400",
+          }}
+        >
+          How it works
+        </h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <div style={{ alignContent: "center" }}>
+              <Image src={require("../../images/Rectanglec.png")} />
+            </div>
+
+            <h3 style={{ textAlign: "center" }}>How It Work</h3>
+            <p style={{ fontWeight: "400", textAlign: "center" }}>
+              Egestas venenatis velit eu odio vitae tristique. Eget sit urna
+              imperdiet id orem
+            </p>
+          </div>
+          <div>
+            <Image src={require("../../images/Rectanglec.png")} />
+            <h3 style={{ textAlign: "center" }}>How It Work</h3>
+            <p style={{ fontWeight: "400", textAlign: "center" }}>
+              Egestas venenatis velit eu odio vitae tristique. Eget sit urna
+              imperdiet id orem
+            </p>
+          </div>
+          <div>
+            <Image src={require("../../images/Rectanglec.png")} />
+            <h3 style={{ textAlign: "center" }}>How It Work</h3>
+            <p style={{ fontWeight: "400", textAlign: "center" }}>
+              Egestas venenatis velit eu odio vitae tristique. Eget sit urna
+              imperdiet id orem
+            </p>
+          </div>
+        </div>
+      </div>
+      <div
+        style={{ marginTop: 100, textAlign: "center", fontFamily: "Work Sans" }}
+      >
+        <h1>Do More With SanwoPay</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas
+          venenatis velit eu odio vitae tristique. Eget sit urna imperdiet id.
+        </p>
+        <div style={{ justifyContent: "center", display: "flex" }}>
+          <Image src={require("../../images/Rectangleb.png")} style={{}} />
+        </div>
+        <div
+          style={{ display: "flex", justifyContent: "center", marginTop: 48 }}
+        >
+          <div
+            style={{
+              backgroundColor: "#C4C4C4",
+              marginRight: 16,
+              width: 225,
+              height: 70,
+              display: "flex",
+              alignItems: "center",
+              borderRadius: 10,
+            }}
+          >
+            <Image
+              src={require("../../images/Rectangled.png")}
+              style={{
+                borderRadius: 10,
+                width: 38,
+                height: 37,
+                marginRight: 5,
+                marginLeft: 17,
+              }}
+            />
+            <div>
+              <h6 style={{ fontSize: 10, fontWeight: 500 }}>Download on the</h6>
+              <h6 style={{ fontSize: 10, fontWeight: 500 }}>App Store</h6>
+            </div>
+          </div>
+          <div
+            style={{
+              backgroundColor: "#C4C4C4",
+              marginRight: 16,
+              width: 225,
+              height: 70,
+              display: "flex",
+              alignItems: "center",
+              borderRadius: 10,
+            }}
+          >
+            <Image
+              src={require("../../images/Rectangled.png")}
+              style={{
+                borderRadius: 10,
+                width: 38,
+                height: 37,
+                marginRight: 5,
+                marginLeft: 17,
+              }}
+            />
+            <div>
+              <h6 style={{ fontSize: 10, fontWeight: 500 }}>Download on the</h6>
+              <h6 style={{ fontSize: 10, fontWeight: 500 }}>Google Store</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          paddingRight: 100,
+          paddingLeft: 100,
+          backgroundColor: "#F6F2F1",
+          display: "flex",
+          marginTop: 200,
+          height: 344,
+          alignItems: "center",
+          fontFamily: "Work Sans",
+        }}
+      >
+        <div style={{ marginRight: 10, display: "flex", alignItems: "center" }}>
+          <Image src={require("../../images/Rectangled.png")} />
+        </div>
+        <div>
+          <p style={{ fontSize: 24, fontWeight: "400", paddingRight: 115 }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas
+            venenatis velit eu odio vitae tristique. Eget sit urna imperdiet id.
+            Lorem ipsum dolor sit amet, consectetur
+          </p>
+          <p style={{ fontSize: 32, fontWeight: "500" }}>Heffret Brown</p>
+          <p>Customer</p>
+        </div>
+        <div style={{ display: "flex" }}>
+          <Image
+            src={require("../../images/Rectanglee.png")}
+            style={{ marginTop: -90, marginRight: 20 }}
+          />
+        </div>
+        <div style={{ marginRight: 10, display: "flex", alignItems: "center" }}>
+          <Image src={require("../../images/Rectangled.png")} />
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          paddingRight: 100,
+          paddingLeft: 100,
+          marginTop: 100,
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <Image
+            src={require("../../images/Rectanglee.png")}
+            // style={{ marginTop: -90, marginRight: 20 }}
+          />
+        </div>
+        <div style={{ textAlign: "left", width: 700 }}>
+          <h2>FAQ's</h2>
         </div>
       </div>
     </div>
